ca7d24fbea02b85e7bd0a27e55451d14b3be3318
vladislav shakhov <shahov199@gmail.com>
2022-12-20 17:01:14 +0300

    zadacha_13

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,11 @@
+﻿int number = new Random().Next (10,1000);
+Console.WriteLine(number);
+if (number >= 100)
+{
+    int thirdNumber = number % 10;
+    Console.WriteLine(thirdNumber);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
