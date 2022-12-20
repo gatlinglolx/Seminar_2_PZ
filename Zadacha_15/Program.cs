1a1234f78585a5601b086b476dfdea3510eee21f
vladislav shakhov <shahov199@gmail.com>
2022-12-20 17:06:22 +0300

    zadacha_15

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,17 @@
+﻿int Monday = 1;
+int Tuesday = 2;
+int Wednesday = 3;
+int Thursday= 4;
+int Friday= 5;
+int Saturday = 6;
+int Sunday = 7;
+int number = new Random().Next (1,8);
+Console.WriteLine(number);
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
